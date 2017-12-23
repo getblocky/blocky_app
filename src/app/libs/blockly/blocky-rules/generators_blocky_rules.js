c8eb9ebaa839be1a93f35735eab59289eeb52446
Phuc Nguyen <phuclx@gmail.com>
2017-12-24 04:07:13 +0700

    Rules - Under constrcution

--- src/app/libs/blockly/blocky-rules/generators_blocky_rules.js
+++ src/app/libs/blockly/blocky-rules/generators_blocky_rules.js
@@ -4,9 +4,9 @@ Blockly.JavaScript['blocky_trigger_device'] = function(block) {
   var dropdown_trigger_device = block.getFieldValue('trigger_device');
   var code = '';
   if (dropdown_trigger_device === 'C') {
-    code = '{"type": "trigger.device.connect"},';
+    code = '"device_register",';
   } else if (dropdown_trigger_device === 'D') {
-    code = '{"type": "trigger.device.disconnect"},';
+    code = '"device_offline",';
   }
   return code;
 };
@@ -15,7 +15,7 @@ Blockly.JavaScript['blocky_trigger_mqtt'] = function(block) {
   var code = '';
   var text_trigger_topic = block.getFieldValue('trigger_topic').trim();
   if (text_trigger_topic.length) {
-    code = '{"type": "trigger.mqtt", "configuration": {"topic": "' + text_trigger_topic + '"}},\n';
+    code = '"mqtt_' + text_trigger_topic + '",';
   }
   return code;
 };
@@ -45,9 +45,7 @@ Blockly.JavaScript['blocky_trigger_timer_schedule'] = function(block) {
 Blockly.JavaScript['blocky_data_mqtt'] = function(block) {
   var text_data_topic = block.getFieldValue('data_topic');
   var dropdown_data_message_type = block.getFieldValue('data_message_type');
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
+  var code = 'getMessageFromTopic("' + text_data_topic + '", "' + dropdown_data_message_type + '")';
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
@@ -55,42 +53,36 @@ Blockly.JavaScript['blocky_action_email'] = function(block) {
   var value_action_email_address = Blockly.JavaScript.valueToCode(block, 'action_email_address', Blockly.JavaScript.ORDER_ATOMIC);
   var value_action_email_subject = Blockly.JavaScript.valueToCode(block, 'action_email_subject', Blockly.JavaScript.ORDER_ATOMIC);
   var value_action_email_message = Blockly.JavaScript.valueToCode(block, 'action_email_message', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '\n';
-  return code;
+  var code = '{"type": "action.email", "configuration": {"email": ' + value_action_email_address + 
+  ', "subject": ' + value_action_email_subject + 
+  ', "message": ' + value_action_email_message + '}},';
+  return code.replace(/'/g, '"');
 };
 
 Blockly.JavaScript['blocky_action_mqtt'] = function(block) {
   var value_action_mqtt_topic = Blockly.JavaScript.valueToCode(block, 'action_mqtt_topic', Blockly.JavaScript.ORDER_ATOMIC);
   var value_action_mqtt_message = Blockly.JavaScript.valueToCode(block, 'action_mqtt_message', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '\n';
-  return code;
+  var code = '{"type": "action.mqtt", "configuration": {"topic": ' + value_action_mqtt_topic + 
+  ', "message": ' + value_action_mqtt_message + '}},';
+  return code.replace(/'/g, '"');
 };
 
 Blockly.JavaScript['blocky_triggers_actions'] = function(block) {
-  var statements_rule_triggers = Blockly.JavaScript.statementToCode(block, 'rule_triggers').trim().slice(0, -1);;
+  var statements_rule_triggers = Blockly.JavaScript.statementToCode(block, 'rule_triggers').trim().slice(0, -1);
   var value_rule_logic = Blockly.JavaScript.valueToCode(block, 'rule_logic', Blockly.JavaScript.ORDER_ATOMIC);
-  
   var blocky_triggers = '<blocky_triggers>[' + statements_rule_triggers + ']</blocky_triggers>\n';
-
-  var blocky_actions = '<blocky_actions>[]</blocky_actions>\n';
-
+  var blocky_actions = '<blocky_actions>' + value_rule_logic + '</blocky_actions>\n';
   var code = blocky_triggers + blocky_actions;
   return code;
 };
 
 Blockly.JavaScript['blocky_data_device_name'] = function(block) {
-  // TODO: Assemble JavaScript into code variable.
-  var code = '';
-  // TODO: Change ORDER_NONE to the correct strength.
+  var code = 'getDeviceName()';
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['blocky_actions'] = function(block) {
-  var statements_rule_actions = Blockly.JavaScript.statementToCode(block, 'rule_actions');
-  // TODO: Assemble JavaScript into code variable.
-  var code = '';
-  // TODO: Change ORDER_NONE to the correct strength.
+  var statements_rule_actions = Blockly.JavaScript.statementToCode(block, 'rule_actions').trim().slice(0, -1);
+  var code = '[' + statements_rule_actions + ']';
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
